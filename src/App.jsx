9854f7a41f7566e256af842f3d66be86e005c3cb
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-08 16:28:26 -0500

    got friggin header to friggin stick

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 
 // This URL can be combined with an photo id to fetch an photo.
-                                      // 200 is the image width
+// 200 is the image width when paired with another number (200/1000 = 200w/1000h); makes square image
 const PHOTO_URL = "https://picsum.photos/200?photo="; // img tag src img 
-// This URL can be used to get an array of objects that contain information
-// about various photos.
+// This URL can be used to get an array of objects that contain information about various photos.
 const PHOTO_LIST_URL = "https://picsum.photos/list"; // first inside fetch 
 
 class App extends Component {
@@ -14,8 +13,7 @@ class App extends Component {
     photos: [] // if empty no err but just maps an emtpy thing; if not arr, err
   };
 
-  // 2. Declare a life cycle method
-  // This life cycle method should:
+  // 2. Declare a life cycle method. This life cycle method should:
   // - will be called after the component is initially rendered
   // - will fetch an array of photos
   // - will add that array of photos to state once received
@@ -36,9 +34,7 @@ class App extends Component {
           <h1>Photo Wall</h1>
         </header>
         <div className="collage">
-            {/* We use map here because Array.prototype.map is an expression,
-              * and for loops are not. You'll learn more about this soon! 
-              */}
+            {/* We use map here because Array.prototype.map is an expression, and for loops are not. You'll learn more about this soon! */}
                       {/* changing all instrances of photo (here and in url) makes the photos different; same with adding a ? or ?random= between phto_url and photo.id. why? is it cause fo the weird url and it's like wtf do i do? */}
             {photos.map(photo => 
                 <img alt={/* 3. Fill me in with the photo's filename */ photo.filename}
